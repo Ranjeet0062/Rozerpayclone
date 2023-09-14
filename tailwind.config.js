5adde2adfeeeeca7835b9c5f1710ee70c14080e2
Ranjeet0062 <odedararanjeet744@gmail.com>
2023-09-14 19:08:47 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mullish: ["Mulish", "sans-serif"],
+      },
+      colors:{
+        deepBlue: "#02042a",
+        lightBlue300: "#4b94ed",
+        lightBlue: "#2b84ea",
+        grayText: "#818597",
+        lightBlue500: "#0b72e7",
+        grayBlue: "#344a6c",
+        greenLight: "#61cea6",
+        deepBlueHead: "#162£56",
+        lightGray: "#e2e2e2",
+        gray2: "#525a76",
+      },
+    },
+  },
+  plugins: [],
+}
+
